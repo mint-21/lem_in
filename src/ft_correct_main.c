dad50d44131e0e06df223c2f03f3704e3d1a60b5
Veronika Fearless <vfearles@mi-w5.msk.21-school.ru>
2020-09-21 12:46:11 +0300

    zerodata

--- src/ft_correct_main.c
+++ src/ft_correct_main.c
@@ -12,44 +12,6 @@
 
 #include "lemin.h"
 
-static void		ft_zerodata2(t_data *data)
-{
-	data->map_data = NULL;
-	data->split = NULL;
-	data->start = NULL;
-	data->end = NULL;
-	data->rooms = NULL;
-	data->options = NULL;
-	data->best_opt = NULL;
-	data->ways_dij = NULL;
-	data->flags.fd_path = NULL;
-}
-
-t_data		ft_zerodata(void)
-{
-	t_data	data;
-
-	data.ants = 0;
-	data.step = 0;
-	data.flag = 1;
-	data.rooms_count = 0;
-	data.total_ways = 0;
-	data.links_count = 0;
-	ft_zerodata2(&data);
-	data.flags.ways = 0;
-	data.check.valid_flag = 0;
-	data.check.li_room_begin = 0;
-	data.check.li_room_finish = 0;
-	data.check.li_connects_bigin = 0;
-	data.check.li_connects_finish = 0;
-	data.check.hash_start = 0;
-	data.check.hash_end = 0;
-	data.links = NULL;
-	data.vis_quit = 0;
-	data.vis_pause = 0;
-	return (data);
-}
-
 /*
 ** ft_create: main funct to init and write info about rooms
 ** ft_connects: основная функция связи между комнатами
---
+++ src/ft_zerodata.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_zerodata.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asmall <asmall@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 20:06:00 by asmall            #+#    #+#             */
+/*   Updated: 2020/09/12 13:40:22 by asmall           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lemin.h"
+
+static void		ft_zerodata2(t_data *data)
+{
+	data->map_data = NULL;
+	data->split = NULL;
+	data->start = NULL;
+	data->end = NULL;
+	data->rooms = NULL;
+	data->options = NULL;
+	data->best_opt = NULL;
+	data->ways_dij = NULL;
+	data->flags.fd_path = NULL;
+}
+
+t_data		ft_zerodata(void)
+{
+	t_data	data;
+
+	data.ants = 0;
+	data.step = 0;
+	data.flag = 1;
+	data.rooms_count = 0;
+	data.total_ways = 0;
+	data.links_count = 0;
+	ft_zerodata2(&data);
+	data.flags.ways = 0;
+	data.check.valid_flag = 0;
+	data.check.li_room_begin = 0;
+	data.check.li_room_finish = 0;
+	data.check.li_connects_bigin = 0;
+	data.check.li_connects_finish = 0;
+	data.check.hash_start = 0;
+	data.check.hash_end = 0;
+	data.links = NULL;
+	data.vis_quit = 0;
+	data.vis_pause = 0;
+	return (data);
+}
