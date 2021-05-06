af1231e73bd288d92ee904146f452637c5d0c9d0
AveiraS <hvissa@yandex.ru>
2021-05-06 19:06:14 +0300

    strsplit сука

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: asmall <asmall@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 19:04:09 by vfearles          #+#    #+#             */
-/*   Updated: 2021/05/06 18:15:07 by asmall           ###   ########.fr       */
+/*   Updated: 2021/05/06 19:05:34 by asmall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,16 +63,13 @@ static void	*matrdel(char **array)
 	return (NULL);
 }
 
-char	**ft_strsplit(char const *s, char c)
+static char	**array_procces(char const *s, char c, char **array)
 {
-	char			**array;
 	size_t			size;
 	size_t			i;
 	char			*str;
 
 	i = 0;
-	if (s == NULL || !(array = (char **)matrm(sizeof(char *) * (g(s, c) + 1))))
-		return (NULL);
 	while (*s != '\0')
 	{
 		while (*s == c)
@@ -80,14 +77,30 @@ char	**ft_strsplit(char const *s, char c)
 		if (*s == '\0')
 			break ;
 		size = ft_strlen(s);
-		if ((str = ft_strchr(s, c)) != NULL)
+		str = ft_strchr(s, c);
+		str = ft_strchr(s, c);
+		if (str != NULL)
 			size = str - s;
-		if (!(array[i] = (char *)ft_memalloc(sizeof(char) * (size + 1))))
+		array[i] = (char *)ft_memalloc(sizeof(char) * (size + 1));
+		if (!(array[i]))
 			return (matrdel(array));
 		ft_strncpy(array[i++], s, size);
 		s = s + size;
 		if (*s != '\0')
 			s++;
 	}
+	return (array);
+}
+
+char	**ft_strsplit(char const *s, char c)
+{
+	char			**array;
+
+	if (s == NULL)
+		return (NULL);
+	array = (char **)matrm(sizeof(char *) * (g(s, c) + 1));
+	if (!(array))
+		return (NULL);
+	array_procces(s, c, array);
 	return (array);
 }
